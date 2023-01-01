831f15b02e4280d0c15db410203d7cb4d7131273
humam-hossain <humam.hossain.e@gmail.com>
2023-01-01 12:16:13 +0600

    template

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
-import '../styles/globals.css'
+import {ThemeProvider} from 'styled-components';
+import withDarkMode, { useDarkMode } from 'next-dark-mode';
+import { lightTheme, darkTheme } from '../themes/default';
+import GlobalStyles from "../styles/globals";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }) {
+  const { darkModeActive } = useDarkMode();
+  const theme = darkModeActive ? darkTheme : lightTheme;
+
+  return (
+    <>
+      <ThemeProvider theme={{darkMode: darkModeActive, ...theme}}>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
+
+export default withDarkMode(App);
